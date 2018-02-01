7893035584dba447187c8dc1ca0ecb663afad6bf
Gaga <g.dzodzuashvili@mail.ru>
2018-02-01 21:40:44 +0300

    Starting new project

---
+++ Starting project/Starting project/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Starting project
+//
+//  Created by Gaga on 01.02.2018.
+//  Copyright © 2018 Gaga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let name = "gaga"
+        print(name)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
